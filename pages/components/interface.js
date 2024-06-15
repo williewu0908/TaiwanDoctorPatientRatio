43383b9940270edcb4e3637344b24c958f347673
威廷的天選四\v60ca <williewu0908@gmail.com>
2024-06-15 22:26:28 +0800

    刪掉fetch csv

--- pages/components/interface.js
+++ pages/components/interface.js
@@ -1,67 +1,38 @@
 import TaiwanSvg from './Taiwan.svg';
 import styles from './interface.module.css';
-import { useEffect, useState } from 'react';
-import csvToJson from 'csvtojson';
+import { useState } from 'react';
 import Title from './Title';
 import Tooltip from './Tooltip';
 
-const csvFilePath = './DoctorPeopleRatio.csv';
-
 const defaultPlaceData = [
-  { tag: "taipei_city", 地區: "臺北市", 每萬人西醫數: 11.35 },
-  { tag: "new_taipei_city", 地區: "新北市", 每萬人西醫數: 4.87 },
-  { tag: "taichung_city", 地區: "臺中市", 每萬人西醫數: 3.87 },
-  { tag: "tainan_city", 地區: "臺南市", 每萬人西醫數: 5.29 },
-  { tag: "kaohsiung_city", 地區: "高雄市", 每萬人西醫數: 9.2 },
-  { tag: "keelung_city", 地區: "基隆市", 每萬人西醫數: 5.29 },
-  { tag: "taoyuan_country", 地區: "桃園市", 每萬人西醫數: 5.29 },
-  { tag: "hsinchu_city", 地區: "新竹市", 每萬人西醫數: 5.29 },
-  { tag: "hsinchu_country", 地區: "新竹縣", 每萬人西醫數: 5.29 },
-  { tag: "miaoli_country", 地區: "苗栗縣", 每萬人西醫數: 5.29 },
-  { tag: "changhua_country", 地區: "彰化縣", 每萬人西醫數: 5.29 },
-  { tag: "nantou_country", 地區: "南投縣", 每萬人西醫數: 5.29 },
-  { tag: "yunlin_country", 地區: "雲林縣", 每萬人西醫數: 5.29 },
-  { tag: "chiayi_city", 地區: "嘉義市", 每萬人西醫數: 5.29 },
-  { tag: "chiayi_country", 地區: "嘉義縣", 每萬人西醫數: 5.29 },
-  { tag: "pingtung_country", 地區: "屏東縣", 每萬人西醫數: 5.29 },
-  { tag: "yilan_country", 地區: "宜蘭縣", 每萬人西醫數: 6.23 },
-  { tag: "hualien_country", 地區: "花蓮縣", 每萬人西醫數: 2.43 },
-  { tag: "taitung_country", 地區: "臺東縣", 每萬人西醫數: 2.21 },
-  { tag: "penghu_country", 地區: "澎湖縣", 每萬人西醫數: 2.92 },
-  { tag: "kinmen_country", 地區: "金門縣", 每萬人西醫數: 2.3 },
-  { tag: "lienchiang_country", 地區: "連江縣", 每萬人西醫數: 1.45 }
+  { tag: "taipei_city", 地區: "臺北市", 每萬人西醫數: 45.1 },
+  { tag: "new_taipei_city", 地區: "新北市", 每萬人西醫數: 15.69 },
+  { tag: "taichung_city", 地區: "臺中市", 每萬人西醫數: 24.37 },
+  { tag: "tainan_city", 地區: "臺南市", 每萬人西醫數: 21.37 },
+  { tag: "kaohsiung_city", 地區: "高雄市", 每萬人西醫數: 26.03 },
+  { tag: "keelung_city", 地區: "基隆市", 每萬人西醫數: 21.59 },
+  { tag: "taoyuan_country", 地區: "桃園市", 每萬人西醫數: 19.5 },
+  { tag: "hsinchu_city", 地區: "新竹市", 每萬人西醫數: 21.23 },
+  { tag: "hsinchu_country", 地區: "新竹縣", 每萬人西醫數: 12.3 },
+  { tag: "miaoli_country", 地區: "苗栗縣", 每萬人西醫數: 10.94 },
+  { tag: "changhua_country", 地區: "彰化縣", 每萬人西醫數: 17.49 },
+  { tag: "nantou_country", 地區: "南投縣", 每萬人西醫數: 13.73 },
+  { tag: "yunlin_country", 地區: "雲林縣", 每萬人西醫數: 13.55 },
+  { tag: "chiayi_city", 地區: "嘉義市", 每萬人西醫數: 35.81 },
+  { tag: "chiayi_country", 地區: "嘉義縣", 每萬人西醫數: 18.14 },
+  { tag: "pingtung_country", 地區: "屏東縣", 每萬人西醫數: 14.79 },
+  { tag: "yilan_country", 地區: "宜蘭縣", 每萬人西醫數: 15.91 },
+  { tag: "hualien_country", 地區: "花蓮縣", 每萬人西醫數: 28.38 },
+  { tag: "taitung_country", 地區: "臺東縣", 每萬人西醫數: 15.37 },
+  { tag: "penghu_country", 地區: "澎湖縣", 每萬人西醫數: 11.94 },
+  { tag: "kinmen_country", 地區: "金門縣", 每萬人西醫數: 6.43 },
+  { tag: "lienchiang_country", 地區: "連江縣", 每萬人西醫數: 13.19 }
 ];
 
 export default function Interface() {
-  const [dataLoaded, setDataLoaded] = useState(false);
-  const [placeData, setPlaceData] = useState(defaultPlaceData);
-  const [ratio, setRatio] = useState(0.21);
+  const [placeData] = useState(defaultPlaceData);
+  const [ratio, setRatio] = useState(22.3);
   const [tooltip, setTooltip] = useState({ display: false, x: 0, y: 0, content: '' });
-
-  useEffect(() => {
-    const updateData = async () => {
-      try {
-        const response = await fetch(csvFilePath);
-        const text = await response.text();
-        const json = await csvToJson().fromString(text);
-
-        const updatedPlaceData = defaultPlaceData.map(place => {
-          const csvRow = json.find(row => row.地區 === place.地區);
-          return csvRow
-            ? { ...place, 每萬人西醫數: parseFloat(csvRow.每萬人西醫數) }
-            : place;
-        });
-
-        setPlaceData(updatedPlaceData);
-        setDataLoaded(true);
-      } catch (error) {
-        console.error('Error loading CSV data:', error);
-        setDataLoaded(true); // In case of error, we still set dataLoaded to true to hide the loading indicator
-      }
-    };
-
-    updateData();
-  }, []);
 
   const handlePathClick = (dataname) => {
     const result = placeData.find(obj => obj.tag === dataname);
@@ -93,10 +64,6 @@ export default function Interface() {
   const handleMouseOut = () => {
     setTooltip({ display: false, x: 0, y: 0, content: '' });
   };
-
-  if (!dataLoaded) {
-    return <div>Loading...</div>; // Show loading indicator while data is being loaded
-  }
 
   return (
     <>
